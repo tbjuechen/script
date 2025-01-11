43fe97a34fbb9b4f02f65af52753110594a8d71b
tbjuechen <1324374092@qq.com>
2025-01-11 18:04:49 +0800

    chore: adjust performer to runner

--- performer/__init__.py
+++
@@ -1,7 +0,0 @@
-from .YYH import YYHPerformer
-from .YH import MultiYHPerformer
-from .Activity import ActivityPerformer
-from .YL import YLPerformer
-from .YJ import YJExpPerformer
-
-scripts:list = [YYHPerformer, MultiYHPerformer, ActivityPerformer, YLPerformer, YJExpPerformer]
--- runner/simple.py
+++ runner/simple.py
@@ -2,16 +2,37 @@
 
 from .base import FindListRunner
 
-class active(FindListRunner):
+class Active(FindListRunner):
     name = 'active'
-    description = 'for monthly active'
+    description = '月伴生活动'
     targets = ['refuse.jpg','active_begin.jpg', 'yys_jixu.jpg', 'yys_jieshu.jpg']
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
 class Mitama(FindListRunner):
     name = 'Mitama'
-    description = 'for Mitama'
+    description = '多人御魂副本'
     targets =  ['refuse.jpg','yys_begin.jpg', 'yys_jieshu.jpg', 'yys_jixu.jpg']
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class HreoExp(FindListRunner):
+    name = 'HeroExp'
+    description = '英杰经验副本'
+    targets = ['refuse.jpg','yj_exp_begin.jpg', 'yys_jieshu.jpg', 'yys_jixu.jpg']
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class Spirit(FindListRunner):
+    name = 'Spirit'
+    description = '御灵副本'
+    targets = ['refuse.jpg','confirm.jpg','yuling_begin.jpg', 'yys_jieshu.jpg', 'yys_jixu.jpg']
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class Fire(FindListRunner):
+    name = 'Fire'
+    description = '业原火'
+    targets = ['refuse.jpg','yyh_begin.jpg', 'yys_jieshu.jpg', 'yys_jixu.jpg']
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
